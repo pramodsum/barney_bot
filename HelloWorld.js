695917c7cbbb5e720954f01ecbbe885b5893f2d3
Sumedha <pramods@umich.edu>
2014-01-29 16:06:32 -0500

    Creating Broda

--- HelloWorld.js
+++ HelloWorld.js
@@ -250,25 +250,26 @@ incoming.on('message', function(msg) {
                 "X-Mashape-Authorization": "iR2g3eyxXH6tK1tZELkkVJikSMeafCWC"
               })
               .end(function (response) {
-                console.log(response);
-                // var url = "http://brospeak.com/?api=yeah&input=" + txt;
-                // var Request = unirest.get(url)
-                //   .end(function (response) {
-                //     console.dir("Text: " + txt + "\nBroSpeak: " + response.body);
+                console.log(response.body);
+                txt = response.body;
+                var url = "http://brospeak.com/?api=yeah&input=" + txt;
+                var Request = unirest.get(url)
+                  .end(function (response) {
+                    console.dir("Text: " + txt + "\nBroSpeak: " + response.body);
 
-                //     API.Bots.post(
-                //         ACCESS_TOKEN, // Identify the access token
-                //         bot_id, // Identify the bot that is sending the message
-                //         response.body, // Construct the message
-                //         {}, // No pictures related to this post
-                //         function(err,res) {
-                //             if (err) {
-                //                 console.log("[API.Bots.post] Reply Message Error!");
-                //             } else {
-                //                 console.log("[API.Bots.post] Reply Message Sent!");
-                //             }
-                //         });
-                //   });
+                    API.Bots.post(
+                        ACCESS_TOKEN, // Identify the access token
+                        bot_id, // Identify the bot that is sending the message
+                        response.body, // Construct the message
+                        {}, // No pictures related to this post
+                        function(err,res) {
+                            if (err) {
+                                console.log("[API.Bots.post] Reply Message Error!");
+                            } else {
+                                console.log("[API.Bots.post] Reply Message Sent!");
+                            }
+                        });
+                  });
               });
             }
         }
