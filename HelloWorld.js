c59f11b2c861b85895bc78205306ceaf177e40b4
Sumedha <pramods@umich.edu>
2014-01-28 21:57:11 -0500

    adding heroku server stuff

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,3 +1,18 @@
+var express = require("express");
+var logfmt = require("logfmt");
+var app = express();
+
+app.use(logfmt.requestLogger());
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
 var GroupMe = require('groupme');
 var API = GroupMe.Stateless;
 
