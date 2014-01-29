c842dff0b8fc1d778a8746b4148dca8d2507fe3e
Sumedha <pramods@umich.edu>
2014-01-28 21:11:43 -0500

    added init web.js code back

--- web.js
+++ web.js
@@ -1,3 +1,8 @@
+
+var express = require("express");
+var logfmt = require("logfmt");
+var app = express();
+
 var bot = require('fancy-groupme-bot');
 var util = require('util');
 
@@ -25,3 +30,14 @@ mybot.on('botMessage', function(b, message) {
 
 console.log("i am serving");
 mybot.serve(8000);
+
+app.use(logfmt.requestLogger());
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
